4190ada75b8a3a3ae5b92d64d18f4295f7e0bc2d
arno-dutra <119803640+arno-dutra@users.noreply.github.com>
2023-07-18 23:43:23 +0200

    Faster way for nav positionning

--- nav.js
+++ nav.js
@@ -1,5 +1,11 @@
-$(window).scroll(function(){
-    $('#nav').css({
-        'left': $(this).scrollLeft() + window.screen.width * 0.67 //Why this 15, because in the CSS, we have set left 15, so as we scroll, we would want this to remain at 15px left
-    });
+//$(window).scroll(function(){
+//    $('#nav').css({
+//        'left': $(this).scrollLeft() + window.screen.width * 0.67 //Why this 15, because in the CSS, we have set left 15, so as we scroll, we would want this to remain at 15px left
+//    });
+//});
+$(window).on('scroll', function () {
+
+  var $w = $(window);
+  $('#nav').css('left', $w.scrollLeft());
+
 });
