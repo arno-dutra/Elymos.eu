750342296adbb603c49e7b9f978782c428fca822
arno-dutra <119803640+arno-dutra@users.noreply.github.com>
2023-07-18 22:22:59 +0200

    Elements are now loaded from a json file

---
+++ gallerie.js
@@ -0,0 +1,66 @@
+
+console.log(window.contenu);
+var content = JSON.parse(window.contenu);
+put_elements(content);
+
+
+
+function put_elements(content) {
+	
+	
+
+	var keys = Object.keys(content);
+	console.log(keys);
+	for (var i=0; i < keys.length; i++) {
+		var k = keys[i];
+		
+		make_element(content[k], k);
+		
+	}
+	
+}
+
+function make_element(element, id) {
+	switch (element.type) {
+		case "image":
+			make_image(element, id);
+			break;
+		default:
+			alert(`element's type ${element.type} is not recognized`);
+	}
+}
+
+function make_image(element, id) {
+	var div = document.createElement('div');
+	var espace = document.getElementById("espace_fil");
+	espace.appendChild(div);
+	
+	var img = document.createElement('img');
+	
+	img.style.left = element.left;
+	img.style.top = element.top;
+	img.id = id;
+	img.classList.add("panneau");
+	img.classList.add(`panneau_${element.espace}`);
+	img.src = `img/${element.espace}/${element.src}`;
+	div.appendChild(img);
+	
+	if (typeof element.legend !== 'undefined') {
+		var legend = document.createElement('h2');
+		legend.style.left = (eval(element.left.replace("px", "")) + 75).toString().concat("px");
+		legend.style.top = (eval(element.top.replace("px", "")) - 20).toString().concat("px");
+		legend.id = id + "-legend";
+		legend.classList.add("legend");
+		legend.classList.add(`legend_${element.espace}`);
+		legend.innerHTML = element.legend;
+		div.appendChild(legend);
+	}
+}
+
+
+
+
+
+
+
+
