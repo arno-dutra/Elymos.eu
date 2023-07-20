5589428bddbf50cf327dfffdb32580475a80b973
arno-dutra <119803640+arno-dutra@users.noreply.github.com>
2023-07-20 22:50:56 +0200

    Fixing background tiles' manager
    Changing the conditions to update the tiles
    Fixing the buffer where unused tiles are stored
    Renaming gallerie.js to espace.js

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
 // JavaScript Document
-
-const hexToRgb = hex =>
-  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-    .substring(1).match(/.{2}/g)
-    .map(x => parseInt(x, 16))
+//
+//const hexToRgb = hex =>
+//  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+//             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+//    .substring(1).match(/.{2}/g)
+//    .map(x => parseInt(x, 16))
 
 
 function onLoad() {
-
-	var body = document.body,
-    html = document.documentElement;
-
-	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-	var width = Math.max( body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth );
+//
+//	var body = document.body,
+//    html = document.documentElement;
+//
+//	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+//	var width = Math.max( body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth );
+	var height = window.screen.height;
+	var width = window.screen.width;
 	
 	window.tile_size = 256;
 	
@@ -28,25 +30,14 @@ function onLoad() {
 	window.n_tiles_x = Math.ceil((width + window.buffer.margin) / window.tile_size) + 3;
 	window.n_tiles_y = Math.ceil((height + window.buffer.margin) / window.tile_size) + 3;
 	
-	var color = "#141111";
+	var color = "#BB1313";
 	document.getElementById("body").style.backgroundColor = color;
 	
-	var background = document.getElementById("background");
+	window.elements_a_recycler = [];
 	
 	// manipulate tiles
-	for(var y = 0; y < window.n_tiles_y; y++) {
-		for(var x = 0; x < window.n_tiles_x; x++) {
-			var pos = (y * window.n_tiles_y + x); // position in buffer based on x and y
-			var tile = createTile();
-			tile.id = "background_tile_".concat(pos.toString());
-			var left = window.buffer.x1 + (x - 1) * window.tile_size;
-			tile.style.left = left.toString().concat("px");
-			var top = window.buffer.y1 + (y - 1) * window.tile_size;
-			tile.style.top = top.toString().concat("px");
-			background.appendChild(tile);
-			
-		}
-	}
+	updateBackground(true)
+	
 	
 	window.addEventListener("resize", onResize)
 	window.addEventListener("scroll", onScroll)
@@ -66,13 +57,13 @@ function onResize() {
 	window.n_tiles_x = Math.ceil((width + window.buffer.margin) / window.tile_size) + 3;
 	window.n_tiles_y = Math.ceil((height + window.buffer.margin) / window.tile_size) + 3;
 	
-	updateBackground()
+	updateBackground(false)
 }
 function onScroll() {
-	updateBackground()
+	updateBackground(false)
 }
 
-function updateBackground() {
+function updateBackground(empty) {
 	// Il faut ajouter au buffer et retirer du buffer les nouvelles couleurs
 	// Ajouter les couleurs qui doivent etre generees en gardant toujours une marge "margin"
 	// Retirer les couleurs qui sont maintenant au delà de la marge "margin" pour ne pas surcharger la mémoire
@@ -82,13 +73,9 @@ function updateBackground() {
 	var x2 = x1 + window.screen.height;
 	var y2 = y1 + window.screen.width;
 	
-	if (x1 < window.buffer.x1 - window.buffer.margin) {
+	if (empty) {
 		_updateBackground(x1, y1, x2, y2)
-	} else if (y1 < window.buffer.y1 - window.buffer.margin) {
-		_updateBackground(x1, y1, x2, y2)
-	} else if (x2 > window.buffer.x2 + window.buffer.margin) {
-		_updateBackground(x1, y1, x2, y2)
-	} else if (y2 < window.buffer.y2 + window.buffer.margin) {
+	} else if ((x1 % window.buffer.margin == 0) & (y1 % window.buffer.margin == 0)) {
 		_updateBackground(x1, y1, x2, y2)
 	}
 //	
@@ -102,48 +89,52 @@ function _updateBackground(x1, y1, x2, y2) {
 	var x0 = 0;
 	var y0 = 0;
 	
-	var elements_a_recycler = [];
-	
 	// Remove tiles too far
 	var tiles = document.getElementsByClassName("background_tile");
-	for (i = 0; i < tiles.length; i++) {
+	for (var i = 0; i < tiles.length; i++) {
 	
 		var tile = tiles.item(i);
-		var left = eval(tile.style.left.replace("px", ""));
-		var top = eval(tile.style.top.replace("px", ""));
-
-		if (left + window.tile_size < x1 - window.buffer.margin) {  // too at left
-			elements_a_recycler.push(tile.id);
-		} else if (top + window.tile_size < y1 - window.buffer.margin) {  // too on top
-			elements_a_recycler.push(tile.id);
-		} else if (left > x2 + window.buffer.margin) {  // too at right
-			elements_a_recycler.push(tile.id);
-		} else if (top > y2 + window.buffer.margin) {  // too at bottom
-			elements_a_recycler.push(tile.id);
-		} else {
-			x0 = left;
-			y0 = top;
+		
+		if (!window.elements_a_recycler.includes(tile.id)) {
+			var left = eval(tile.style.left.replace("px", ""));
+			var top = eval(tile.style.top.replace("px", ""));
+			
+			if (left + window.tile_size < x1 - window.buffer.margin) {  // too at left
+				window.elements_a_recycler.push(tile.id);
+			} else if (top + window.tile_size < y1 - window.buffer.margin) {  // too on top
+				window.elements_a_recycler.push(tile.id);
+			} else if (left > x2 + window.buffer.margin) {  // too at right
+				window.elements_a_recycler.push(tile.id);
+			} else if (top > y2 + window.buffer.margin) {  // too at bottom
+				window.elements_a_recycler.push(tile.id);
+			} else {
+				x0 = left;
+				y0 = top;
+			}
 		}
+		
+		
 			
 	}
 	
 	
-	_fillWithTiles(x0, y0, x1, y1, x2, y2, elements_a_recycler)
+	_fillWithTiles(x0, y0, x1, y1)
 	
 }
 
-function _fillWithTiles(x0, y0, x1, y1, x2, y2, elements_a_recycler) {
+function _fillWithTiles(x0, y0, x1, y1) {
 	// (x0, y0) are the coordinates of one an existing tile. This will be used to phase the grid
 	
 	
 	var background = document.getElementById("background");
 	
-	
-	var color = "#141111";  // to delete
-	var c = hexToRgb(color);  // to delete
-	
 	var x01 = x1 + ((x0 - x1) % window.tile_size);
 	var y01 = y1 + ((y0 - y1) % window.tile_size);
+	
+	var tile_id = "";
+	var tile = background;  // Only to initialize the variable
+	
+	console.log(window.elements_a_recycler);
 	
 	// manipulate tiles
 	for(var y = 0; y < window.n_tiles_y; y++) {
@@ -153,10 +144,11 @@ function _fillWithTiles(x0, y0, x1, y1, x2, y2, elements_a_recycler) {
 			var id = "background_tile_".concat(left.toString(), "_", top.toString());
 			
 			if (document.getElementById(id) == null) {
-				if (elements_a_recycler.length == 0) {
-					var tile = createTile();
+				if (window.elements_a_recycler.length == 0) {
+					tile = createTile();
 				} else {
-					var tile = elements_a_recycler.pop();
+					tile_id = window.elements_a_recycler.pop();
+					tile = document.getElementById(tile_id);
 				}
 				tile.id = id;
 				tile.style.left = left.toString().concat("px");
@@ -186,18 +178,15 @@ function createTile() {
 	var imgData = ctx.getImageData(0, 0, tile.width, tile.height);
 	tile.data = imgData.data;
 	
-	var color = "#141111";  // to delete
-	var c = hexToRgb(color);  // to delete
-	
 	for(var y = 0; y < tile_size; y++) {
 		for(var x = 0; x < tile_size; x++) {
 			var pos = (y * tile_size + x) * 4; // position in buffer based on x and y
-			var r = (Math.random() - 0.5) * 10;
-			r = Math.min(Math.max(r, 0), 255);
-			tile.data[pos  ] = c[0] + r;           // some R value [0, 255]
-			tile.data[pos+1] = c[1] + r;          // some G value
-			tile.data[pos+2] = c[2] + r;           // some B value
-			tile.data[pos+3] = 255;           // set alpha channel
+			var r = Math.random() * 10;
+			r = Math.min(Math.max(r, 0), 100);
+			tile.data[pos  ] = r;           // some R value [0, 255]
+			tile.data[pos+1] = r;          // some G value
+			tile.data[pos+2] = r;           // some B value
+			tile.data[pos+3] = 5*r;           // set alpha channel
 		}
 	}
 
