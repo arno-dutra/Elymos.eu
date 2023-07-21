6693f9bf9111e46ab0dc1a96426fb354e2807133
arno-dutra <119803640+arno-dutra@users.noreply.github.com>
2023-07-22 00:53:52 +0200

    Creating a function to update the hud color

--- nav.js
+++ nav.js
@@ -1,11 +1,30 @@
-//$(window).scroll(function(){
-//    $('#nav').css({
-//        'left': $(this).scrollLeft() + window.screen.width * 0.67 //Why this 15, because in the CSS, we have set left 15, so as we scroll, we would want this to remain at 15px left
-//    });
-//});
+
+// Make nav following us along x
 $(window).on('scroll', function () {
 
   var $w = $(window);
   $('#nav').css('left', $w.scrollLeft());
 
 });
+
+
+const hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
+
+// Adapt the color of the hud elements depending on espace
+function update_hud_color(espace_courrant) {
+	
+	var hex_color = window.espaces[espace_courrant].backgroundColor;
+	
+	var brightness = Math.max(...hexToRgb(hex_color));
+	
+	if (brightness < 100) {
+		document.documentElement.style.setProperty("--hud-element-color", "#fff");
+	} else {
+		document.documentElement.style.setProperty("--hud-element-color", "#000");
+	}
+	
+}
