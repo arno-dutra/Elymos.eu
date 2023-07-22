af1b2f8383cafe0602dcaf48774b7f28dca1af95
arno-dutra <119803640+arno-dutra@users.noreply.github.com>
2023-07-22 22:24:07 +0200

    Adding static carrousel

--- fill_espace.js
+++ fill_espace.js
@@ -24,6 +24,9 @@ function make_element(element, id) {
 		case "image":
 			make_image(element, id);
 			break;
+		case "minecraft_carrousel":
+			make_minecraft_carrousel(element, id);
+			break;
 		default:
 			alert(`element's type ${element.type} is not recognized`);
 	}
@@ -35,13 +38,17 @@ function make_image(element, id) {
 	var espace = document.getElementById(`espace_fil_${element.espace}`);
 	espace.appendChild(div);
 	
-	var img = document.createElement('img');
+	var img_container = document.createElement('div');
 	
-	img.id = id;
-	img.classList.add("panneau");
-	img.classList.add(`panneau_${element.espace}`);
+	img_container.id = id;
+	img_container.classList.add("panneau");
+	img_container.classList.add(`panneau_${element.espace}`);
+	div.appendChild(img_container);
+	
+	var img = document.createElement('img');
 	img.src = `img/${element.espace}/${element.src}`;
-	div.appendChild(img);
+	img.classList.add("panneau_img");
+	img_container.appendChild(img);
 	
 	if (typeof element.legend !== 'undefined') {
 		var legend = document.createElement('h2');
@@ -55,7 +62,60 @@ function make_image(element, id) {
 	}
 }
 
+function make_minecraft_carrousel(element, id) {
+	var div = document.createElement('div');
+	div.classList.add("group");
+	var espace = document.getElementById(`espace_fil_${element.espace}`);
+	espace.appendChild(div);
+	
+	
+	var carrousel_container = document.createElement('div');
+	carrousel_container.innerHTML = `
+		<table width="100%" border="0" id="espace_${element.espace}_carrousel_container">
+		  <tbody>
+			<tr>
 
+			  <td width="46px" align="center" valign="center">
+				<img id=${id.concat(`-btn-left`)} src=${"img/minecraft_carrousel_arrow_left.svg"} class="panneau_minecraft_arrow panneau_minecraft_arrow_left"
+				</td>
+			  <td width="66%" align="center" valign="top">
+				  <div id="${id}" class="panneau panneau_${element.espace}"></div>
+				</td>
+			  <td width="46px" align="center" valign="center">
+				<img id=${id.concat(`-btn-right`)} src=${"img/minecraft_carrousel_arrow_right.svg"} class="panneau_minecraft_arrow panneau_minecraft_arrow_right"
+				</td>
+			</tr>
+		  </tbody>
+		</table>`;
+	div.appendChild(carrousel_container);
+	
+	var img_container = document.getElementById(id);
+	
+	for (var i = 0; i < element.src.length; i++) {
+		var img = document.createElement('img');
+		img.id = id.concat(`-${i}`)
+		img.src = `img/${element.espace}/${element.src[i]}`;
+		img.classList.add("panneau_img");
+		img.classList.add(`panneau_img_tag-${id}`);
+		img_container.appendChild(img);
+	}	
+	
+	
+	if (typeof element.legend !== 'undefined') {
+		var legend = document.createElement('h2');
+		legend.style.left = "75px";
+		legend.style.top = "-20px";
+		legend.id = id + "-legend";
+		legend.classList.add("legend");
+		legend.classList.add(`legend_${element.espace}`);
+		legend.innerHTML = element.legend;
+		div.appendChild(legend);
+	}
+	
+	
+	
+	
+}
 
 
 
