ffe8f5fca194b5407c3c1ddd08d47be2c86519fb
arno-dutra <arno.dutra@centrale-marseille.fr>
2023-07-30 14:55:05 +0200

    Bluring edges' espace

--- background_color.js
+++ background_color.js
@@ -17,6 +17,7 @@ function load_background_color() {
 			var div = document.createElement('div');
 		div.id = key;
 		div.style.backgroundColor = value.backgroundColor;
+		div.style.boxShadow = `-175px 0px 100px ${value.backgroundColor}`;
 		div.style.left = value.left.toString().concat("px");
 		div.style.top = (value.top + window.espaces.sommaire.height).toString().concat("px");
 		div.style.width = value.width.toString().concat("px");
@@ -32,6 +33,7 @@ function load_background_color() {
 	div = document.createElement('div');
 	div.id = "sommaire";
 	div.style.backgroundColor = window.espaces.sommaire.backgroundColor;
+	div.style.boxShadow = `0px 175px 100px ${window.espaces.sommaire.backgroundColor}`;
 	div.style.left = window.espaces.sommaire.left.toString().concat("px");
 	div.style.top = window.espaces.sommaire.top.toString().concat("px");
 	div.style.width = map_width.toString().concat("px");
