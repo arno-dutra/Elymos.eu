f9cf63849792d42d29cb6a5018d93a04e30278b1
arno-dutra <arno.dutra@centrale-marseille.fr>
2023-10-18 14:52:37 +0200

    Fixing nav current position cursor for nav type span

--- nav_cursor.js
+++ nav_cursor.js
@@ -27,24 +27,33 @@ function allInView() {
 	
 	var elements_du_fil = document.getElementsByClassName(`panneau`);
 
+	var visible_nav_els_set = new Set();
+	
 	for (var i = 0; i < elements_du_fil.length; i++) {
 		
 		var el_content = window.content[elements_du_fil[i].id];
 		
-		if (el_content.nav_type == "timespan") {var nav_el = $(`#${el_content.espace}${el_content.nav_class}_nav`);}
-		else {var nav_el = $(`#${elements_du_fil[i].id}_nav`);}
+		if (el_content.nav_type == "timespan") {var nav_el = document.getElementById(`${el_content.espace}${el_content.nav_class}_nav`);}
+		else {var nav_el = document.getElementById(`${elements_du_fil[i].id}_nav`);}
 		
 		
 		if (isScrolledIntoView(elements_du_fil.item(i))) {
 			
-			nav_el.css("border-width", "3px");
+			visible_nav_els_set.add(nav_el);
 			
-		} else {
-			
-			nav_el.css("border-width", "1px");
+			nav_el.style.borderWidth = "3px";
 			
 		}
 		
 	}
+	
+	[].forEach.call($(".nav_timepoint"), function(nav_el) {
+		if (!visible_nav_els_set.has(nav_el)) {
+			
+			nav_el.style.borderWidth = "1px";
+		}
+	})
+	
+	
     
 }
